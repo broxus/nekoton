5b7d2047a4c728a29920655ce5321615d1f1ff30
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-03-20 19:23:41 +0300

    Storage trait update

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,9 +1,10 @@
-pub mod keystore;
 pub use keystore::mnemonics::*;
 pub use keystore::StoredKey;
 
+pub mod keystore;
+
 pub trait KvStorage {
     type Error;
-    fn get(key: &str) -> Result<String, Self::Error>;
-    fn set(key: &str, value: &str) -> Result<(), Self::Error>;
+    fn get(&self, key: &str) -> Result<String, Self::Error>;
+    fn set(&self, key: &str, value: &str) -> Result<(), Self::Error>;
 }
