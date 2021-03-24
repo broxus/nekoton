88e07d1107149a94f8835ba35e2b978222bcabf5
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-03-24 20:24:43 +0300

    WIP: Refactor

--- src/contracts/wallet/mod.rs
+++ src/contracts/wallet/mod.rs
@@ -1,4 +1,3 @@
-use cached::proc_macro::cached;
 use ton_block::Message;
 
 use crate::transport::Transport;
@@ -10,19 +9,3 @@ mod state;
 pub struct Wallet {
     state: WalletState,
 }
-
-// impl Wallet {
-//     fn new(transport: &dyn Transport) {}
-//
-//     pub fn send_message(&self, message: &Message) {
-//         // self.transport.send_message(message)
-//     }
-//
-//     pub fn get_state(&self) -> &WalletState {
-//         &self.state
-//     }
-//
-//     pub fn update_state(&mut self) -> &WalletState {
-//         &self.state
-//     }
-// }
