316cbbc1e9b2f68ee115e19c5be989bb798600b1
Ivan Kalinin <i.kalinin@dexpa.io>
2021-03-30 23:59:55 +0300

    Improve address packing

--- src/helpers/address.rs
+++ src/helpers/address.rs
@@ -1,6 +1,6 @@
-use anyhow::Error;
+use anyhow::Result;
 use base64::URL_SAFE;
-use ton_block::MsgAddrStd;
+use ton_block::{MsgAddrStd, MsgAddressInt};
 use ton_types::AccountId;
 
 use crate::helpers::crc::crc_16;
@@ -8,7 +8,16 @@ use crate::helpers::crc::crc_16;
 ///Packs std address to base64 format
 /// # Arguments
 /// `base64_url` - encode with url friendly charset or not
-pub fn pack_std_smc_addr(base64_url: bool, addr: &MsgAddrStd, bounceable: bool) -> String {
+pub fn pack_std_smc_addr(
+    base64_url: bool,
+    addr: &MsgAddressInt,
+    bounceable: bool,
+) -> Result<String> {
+    let addr = match addr {
+        MsgAddressInt::AddrStd(addr) => addr,
+        _ => return Err(AddressConversionError::UnsupportedAddressType.into()),
+    };
+
     let testnet = false;
     let mut buffer = [0u8; 36];
     buffer[0] = (0x51 - (bounceable as i32) * 0x40 + (testnet as i32) * 0x80) as u8;
@@ -23,36 +32,49 @@ pub fn pack_std_smc_addr(base64_url: bool, addr: &MsgAddrStd, bounceable: bool) 
         base64::encode(&buffer)
     };
     assert_eq!(b64_enc.len(), 48);
-    b64_enc
+    Ok(b64_enc)
 }
 
 ///Unpacks base64 encoded address to std address
 /// # Arguments
 /// `base64_url` - encode with url friendly charset or not
-pub fn unpack_std_smc_addr(packed: &str, base64_url: bool) -> Result<MsgAddrStd, Error> {
+pub fn unpack_std_smc_addr(packed: &str, base64_url: bool) -> Result<MsgAddressInt> {
     let unpacked = if base64_url {
         base64::decode_config(packed, URL_SAFE)
     } else {
         base64::decode(packed)
     }
-    .map_err(|e| Error::new(e).context("Base64 decode error"))?;
-    anyhow::ensure!(
-        unpacked.len() == 36,
-        "Bad packed address length. Expected 36, got {}",
-        unpacked.len()
-    );
+    .map_err(|_| AddressConversionError::InvalidBase64)?;
+
+    if unpacked.len() != 36 {
+        return Err(AddressConversionError::InvalidPackedLength.into());
+    }
+
     let crc = crc_16(&unpacked[..34]);
     if unpacked[34] as u16 != (crc >> 8) || unpacked[35] as u16 != (crc & 0xff) {
-        anyhow::bail!("Bad crc");
+        return Err(AddressConversionError::InvalidChecksum.into());
     }
+
     let wc = unpacked[1];
     let address = &unpacked[2..34];
     let address = AccountId::from_raw(address.to_vec(), address.len() * 8);
-    Ok(MsgAddrStd {
+    Ok(MsgAddressInt::AddrStd(MsgAddrStd {
         workchain_id: wc as i8,
         anycast: None,
         address,
-    })
+    }))
+}
+
+#[derive(thiserror::Error, Debug)]
+enum AddressConversionError {
+    #[error("Unsupported address type")]
+    UnsupportedAddressType,
+    #[error("Invalid base64")]
+    InvalidBase64,
+    #[error("Invalid packed address length")]
+    InvalidPackedLength,
+    #[error("Invalid checksum")]
+    InvalidChecksum,
 }
 
 #[cfg(test)]
@@ -62,66 +84,62 @@ mod test {
     use crate::helpers::address::{pack_std_smc_addr, unpack_std_smc_addr};
     #[cfg(test)]
     use pretty_assertions::assert_eq;
-    use ton_block::{MsgAddrStd, MsgAddressInt};
+    use ton_block::MsgAddressInt;
 
-    fn get_std_addr() -> MsgAddrStd {
-        if let MsgAddressInt::AddrStd(a) = MsgAddressInt::from_str(
+    fn test_addr() -> MsgAddressInt {
+        MsgAddressInt::from_str(
             "0:02e3f2284e68a8106b823ab9f2404f33cc43fccad8e1de835bdd96789254686c",
         )
         .unwrap()
-        {
-            a
-        } else {
-            unreachable!()
-        }
     }
+
     #[test]
     fn check_pack_std_smc_addr_non_bounce() {
-        let addr = get_std_addr();
+        let addr = test_addr();
         assert_eq!(
-            pack_std_smc_addr(false, &addr, false),
+            pack_std_smc_addr(false, &addr, false).unwrap(),
             "UQAC4/IoTmioEGuCOrnyQE8zzEP8ytjh3oNb3ZZ4klRobAEx"
         );
     }
 
     #[test]
     fn check_pack_std_smc_addr_url_non_bounce() {
-        let addr = get_std_addr();
+        let addr = test_addr();
         assert_eq!(
-            pack_std_smc_addr(true, &addr, false),
+            pack_std_smc_addr(true, &addr, false).unwrap(),
             "UQAC4_IoTmioEGuCOrnyQE8zzEP8ytjh3oNb3ZZ4klRobAEx"
         );
     }
 
     #[test]
     fn check_pack_std_smc_addr_bounce() {
-        let addr = get_std_addr();
+        let addr = test_addr();
         assert_eq!(
-            pack_std_smc_addr(false, &addr, true),
+            pack_std_smc_addr(false, &addr, true).unwrap(),
             "EQAC4/IoTmioEGuCOrnyQE8zzEP8ytjh3oNb3ZZ4klRobFz0"
         );
     }
 
     #[test]
     fn check_pack_std_smc_addr_url_bounce() {
-        let addr = get_std_addr();
+        let addr = test_addr();
         assert_eq!(
-            pack_std_smc_addr(true, &addr, true),
+            pack_std_smc_addr(true, &addr, true).unwrap(),
             "EQAC4_IoTmioEGuCOrnyQE8zzEP8ytjh3oNb3ZZ4klRobFz0"
         );
     }
 
     #[test]
     fn unpack_no_bounce() {
-        let addr = get_std_addr();
-        let packed = pack_std_smc_addr(false, &addr, false);
+        let addr = test_addr();
+        let packed = pack_std_smc_addr(false, &addr, false).unwrap();
         let address = unpack_std_smc_addr(&packed, false).unwrap();
         assert_eq!(addr, address);
     }
     #[test]
     fn unpack_bounce() {
-        let addr = get_std_addr();
-        let packed = pack_std_smc_addr(false, &addr, true);
+        let addr = test_addr();
+        let packed = pack_std_smc_addr(false, &addr, true).unwrap();
         let address = unpack_std_smc_addr(&packed, false).unwrap();
         assert_eq!(addr, address);
     }
