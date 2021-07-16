d6f7c6b8193ef76e96250919077077e6419343b9
Ivan Kalinin <i.kalinin@dexpa.io>
2021-07-16 17:44:39 +0300

    Fix bounced messages parsing

--- src/core/parsing.rs
+++ src/core/parsing.rs
@@ -489,6 +489,9 @@ pub fn parse_token_transaction(
 
     let header = in_msg.int_header()?;
     if header.bounced {
+        body.move_by(32).ok()?;
+        let function_id = read_u32(&body).ok()?;
+
         if function_id == functions.internal_transfer.input_id {
             return Some(TokenWalletTransaction::TransferBounced(
                 body.get_next_u128().ok()?.into(),
@@ -923,6 +926,21 @@ mod tests {
         assert!(matches!(
             parse_token_transaction(&tx, &description, TokenWalletVersion::Tip3v3).unwrap(),
             TokenWalletTransaction::Accept(_)
+        ));
+    }
+
+    #[test]
+    fn test_parse_bounced_tokens_transfer() {
+        let (tx, description) = parse_transaction("te6ccgECCQEAAiEAA7V9jKvgMYxeLukedeW/PRr7QyRzEpkal33nb9KfgpelA3AAAO1mmxCMEy4UbEGiIQKVpE2nzO2Ar32k7H36ni1NMpxrcPorUNuwAADtZo+e3BYO9BHwADRwGMkIBQQBAhcMSgkCmI36GG92AhEDAgBvyYehIEwUWEAAAAAAAAQAAgAAAAKLF5Ge7DorMQ9dbEzZTgWK7Jiugap8s4dRpkiQl7CNEEBQFgwAnkP1TAqiBAAAAAAAAAAAtgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgnIBZa/nTbAD2Vcr8A6p+uT7XD4tLowmBLZEuIHLxU1zbeHGgHFi5dfeWnrNgtL3FHE6zw6ysjTJJI3LFFDAgPi3AgHgCAYBAd8HALFoAbGVfAYxi8XdI868t+ejX2hkjmJTI1LvvO36U/BS9KBvABgzjiRJUfoXsV99CuD/WnKK4QN5mlferMiVbk0Y3Jc3ECddFmAGFFhgAAAdrNNiEYTB3oI+QAD5WAHF6/YBDYNj7TABzedO3/4+ENpaE0PhwRx5NFYisFNfpQA2Mq+AxjF4u6R515b89GvtDJHMSmRqXfedv0p+Cl6UDdApiN+gBhRYYAAAHazSjHIEwd6CFH////+MaQuBAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAEA=");
+
+        println!(
+            "{:?}",
+            parse_token_transaction(&tx, &description, TokenWalletVersion::Tip3v4).unwrap()
+        );
+
+        assert!(matches!(
+            parse_token_transaction(&tx, &description, TokenWalletVersion::Tip3v4).unwrap(),
+            TokenWalletTransaction::TransferBounced(_)
         ));
     }
 }
