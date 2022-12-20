7caf3c8d33ee07faae17dc87c65125f70aa0e1bb
Rexagon <Rexagon@users.noreply.github.com>
2022-12-20 10:24:23 +0000

    deploy: 182452e3f215b65972680c18098eb89afa3c9957

--- implementors/nekoton_abi/token_packer/trait.BuildTokenValue.js
+++ implementors/nekoton_abi/token_packer/trait.BuildTokenValue.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "nekoton_abi":[],
-"nekoton_contracts":[["impl <a class=\"trait\" href=\"nekoton_abi/token_packer/trait.BuildTokenValue.html\" title=\"trait nekoton_abi::token_packer::BuildTokenValue\">BuildTokenValue</a> for <a class=\"struct\" href=\"nekoton_contracts/tip4_1/nft_contract/struct.NftCallbackPayload.html\" title=\"struct nekoton_contracts::tip4_1::nft_contract::NftCallbackPayload\">NftCallbackPayload</a>"]]
+"nekoton_contracts":[["impl BuildTokenValue for <a class=\"struct\" href=\"nekoton_contracts/tip4_1/nft_contract/struct.NftCallbackPayload.html\" title=\"struct nekoton_contracts::tip4_1::nft_contract::NftCallbackPayload\">NftCallbackPayload</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
