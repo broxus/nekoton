aaf0e4b85cc0a61ffa8d99ebab2a92b33163bd0f
Rexagon <Rexagon@users.noreply.github.com>
2023-07-02 14:40:53 +0000

    deploy: 47e4356a35f64d6f0158659b2b7d04f89c2220ef

--- nekoton/core/transactions_tree/sidebar-items.js
+++ nekoton/core/transactions_tree/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["TransactionTreeError"],"struct":["MessageWrapper","StoredAccount","TransactionsTreeStream"]};
+window.SIDEBAR_ITEMS = {"enum":["TransactionTreeError"],"struct":["TransactionsTreeStream"]};
