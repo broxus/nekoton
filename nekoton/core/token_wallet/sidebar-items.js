605b8f4439e39d30cb28ec7cfdc477beccad4e44
Rexagon <Rexagon@users.noreply.github.com>
2024-01-08 21:27:50 +0000

    deploy: 0f1a118bef6b421ab2f64a4a62ba595cbcbfb227

--- nekoton/core/token_wallet/sidebar-items.js
+++ nekoton/core/token_wallet/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["get_token_root_details","get_token_root_details_from_token_wallet","get_token_wallet_details"],"struct":["RootTokenContractState","TokenWallet","TokenWalletContractState"],"trait":["TokenWalletSubscriptionHandler"]};
+window.SIDEBAR_ITEMS = {"fn":["get_token_root_details","get_token_root_details_from_token_wallet","get_token_wallet_details"],"struct":["TokenWallet"],"trait":["TokenWalletSubscriptionHandler"]};
---
+++ nekoton_contracts/tip3_any/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Tip3Error","TokenWalletVersion"],"struct":["RootTokenContractDetails","RootTokenContractState","TokenWalletContractState","TokenWalletDetails"]};
