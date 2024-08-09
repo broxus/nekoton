01b65fff799751cf1e9b3c1c651e84a5e2939529
Rexagon <Rexagon@users.noreply.github.com>
2024-08-09 17:31:02 +0000

    deploy: 63e4dfc50fd577b95b44110f717d9d32c2404e83

---
+++ nekoton_abi/tvm/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ExecutionError"],"fn":["call","call_getter","call_msg"],"struct":["ActionPhaseOutput","BriefBlockchainConfig","VmGetterOutput"],"type":["BehaviorModifiers","StackItem"]};
