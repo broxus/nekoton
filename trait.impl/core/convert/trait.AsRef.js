f592d3911fbd764b39d9a2196084e1bec72f3060
Rexagon <Rexagon@users.noreply.github.com>
2025-02-25 18:01:58 +0000

    deploy: 516bc1a231f15c6bb9444e5f18476a28def8e5e3

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["nekoton",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"nekoton/crypto/struct.PasswordCacheTransaction.html\" title=\"struct nekoton::crypto::PasswordCacheTransaction\">PasswordCacheTransaction</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["nekoton",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"nekoton/crypto/struct.PasswordCacheTransaction.html\" title=\"struct nekoton::crypto::PasswordCacheTransaction\">PasswordCacheTransaction</a>&lt;'a&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/iter/traits/collect/trait.FromIterator.js
+++ trait.impl/core/iter/traits/collect/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["nekoton_abi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;Param&gt; for <a class=\"struct\" href=\"nekoton_abi/struct.TupleBuilder.html\" title=\"struct nekoton_abi::TupleBuilder\">TupleBuilder</a>"]]]]);
+    var implementors = Object.fromEntries([["nekoton_abi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;Param&gt; for <a class=\"struct\" href=\"nekoton_abi/struct.TupleBuilder.html\" title=\"struct nekoton_abi::TupleBuilder\">TupleBuilder</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
