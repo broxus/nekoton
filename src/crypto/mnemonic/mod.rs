7b86cb8f77872af407b7a7c19236887bc02f9b80
Alexey Pashinov <pashinov93@gmail.com>
2025-03-04 19:15:08 +0100

    use account_id to derive TON keys

--- src/crypto/mnemonic/mod.rs
+++ src/crypto/mnemonic/mod.rs
@@ -120,7 +120,7 @@ impl Bip39Path {
 
                 derived.secret()
             }
-            Self::Ton => derive_ed25519_private_key(&seed_bytes, &vec![44, 607, 0]),
+            Self::Ton => derive_ed25519_private_key(&seed_bytes, &vec![44, 607, account_id as u32]),
         };
 
         Ok(derived)
